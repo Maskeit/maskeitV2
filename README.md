cb4d2b58ffde2d160c8f92aae237d1bddbbb3b26
Miguel Alejandre <malejandre@MacBook-Air-de-Miguel.local>
2026-03-27 19:43:39 -0600

    feat: deploy to codeale vps 4

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # maskeitV2
+Version 2.0 del portafolio web
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,1 @@
 export * from './scrollUtils'
-
-// TODO: Add more utility exports here
-// export * from './animationUtils'
-// export * from './domUtils'
